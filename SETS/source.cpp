c4aa7ef011978d7eeba5104558e22b7b194083fb
Ahsan Ali <ehsansha222@gmail.com>
2025-12-15 21:15:14 +0500

    Refactor main function and update set operations

--- SETS/source.cpp
+++ SETS/source.cpp
@@ -2,15 +2,28 @@
 #include"set.h"
 int main() {
 	std::cout<< "hello";
-	
-	set<int> s, s1;
-	std::pair<set<int>::iterator, bool> added = s.insert(12);
-	std::pair<set<int>::iterator, bool> added1 = s.insert(16);
-	std::pair<set<int>::iterator, bool> added2 = s.insert(10);
-	std::pair<set<int>::iterator, bool> added3 = s.insert(8);
-	std::pair<set<int>::iterator, bool> added4 = s.insert(20);
 
-	s1 = s;
+	set<int> s, s1;
+	std::pair<set<int>::iterator, bool> added = s.insert(20);
+	s.insert(10);
+	s.insert(30);
+	s.insert(5);
+	s.insert(15);
+	s.insert(25);
+	s.insert(35);
+	s.insert(1);
+	s.insert(6);
+	s.inorder();
+	auto itr = s1.begin();
+	s.erase(itr);
+	s.level_order();
+	
+	//std::pair<set<int>::iterator, bool> added1 = s.insert(16);
+	//std::pair<set<int>::iterator, bool> added2 = s.insert(10);
+	//std::pair<set<int>::iterator, bool> added3 = s.insert(8);
+	//std::pair<set<int>::iterator, bool> added4 = s.insert(20);
+
+	//s1 = s;
 	//std::cout << s.size() << std::endl;
 
 
@@ -26,13 +39,15 @@ int main() {
 	//std::cout << " value " << *it;
 
 	// print full tree
-	auto itr = s1.begin();
-	while ( itr != s1.end() )
-	{
-		std::cout << " val " << *itr;
-		++itr;
+	//auto itr = s1.begin();
+	//++itr;
+	//while ( itr != s1.end() )
+	//{
+	//	std::cout << " val " << *itr;
+	//	++itr;
 
-	}
+	//}
+	
 	//auto eras = s.erase(added3.first);
 	//std::cout << "meow "<< *eras<<std::endl;
 	//auto  itr= s.begin();
