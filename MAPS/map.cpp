7b73f9ad18e01dc4645a6c696617d2d3507be9cf
Ahsan Ali <ehsansha222@gmail.com>
2025-12-20 01:21:45 +0500

    Update map.cpp

--- MAPS/map.cpp
+++ MAPS/map.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+#include"map.h"
+int main() {
+	std::cout << "meow";
+	map<int, int> m;
+	m.insert({1,1});
+	m.insert({ 2,4 });
+	m.insert({ 3,9 });
+	std::cout <<" value: " << m.at(2) << std::endl;
+	m[3] = 10;
+
+}
 
